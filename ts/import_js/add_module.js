e0a0faece4cda9003ddfddb75ba702ea1799e4fb
Nakada Takumi <Twitter@KaeruVim>
2016-04-27 20:31:43 +0900

    AddModule

---
+++ ts/import_js/add_module.js
@@ -0,0 +1,7 @@
+var AddModule = {
+    add: function (n1, n2) {
+        return n1 + n2;
+    }
+}
+
+module.exports = AddModule;
---
+++ ts/import_js/main.ts
@@ -0,0 +1,4 @@
+/// <reference path="add_module.d.ts" />
+import * as AddModule from 'add_module';
+let added = AddModule.add(1, 2);
+console.log(added);
