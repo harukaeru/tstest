f02bef227d51b6b430e10f395bb109ef87e2d3a2
Nakada Takumi <Twitter@KaeruVim>
2016-04-27 12:47:50 +0900

    ReadModule main.ts

--- ts/main.ts
+++ ts/main.ts
@@ -1,3 +1,4 @@
 import * as Ex from './ex.ts';
 console.log(Ex);
 console.log(1);
+console.log('gw');
--- ts/read_module/main.ts
+++ ts/read_module/main.ts
@@ -1,3 +1,11 @@
 import * as Sub from "./sub.ts";
 
 console.log(Sub);
+
+import { C } from "./sub.ts";
+
+console.log(C);
+
+import { A as SubA } from "./sub.ts";
+
+console.log(SubA);
